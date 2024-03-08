7f4e752c228636ec987a23ea6113cc751349be31
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-03-08 14:36:19 +0530

    supratim531: Added new scripts folder and nginx conf file

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,20 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-# kill any servers that may be running in the background 
-sudo pkill -f runserver
-
-# kill frontend servers if you are deploying any frontend
-# sudo pkill -f tailwind
-# sudo pkill -f node
-
-cd /home/ubuntu/trans-crm-cicd/
-
-# activate virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-install requirements.txt
-pip install -r /home/ubuntu/trans-crm-cicd/requirements.txt
-
-# run server
-screen -d -m python3 manage.py runserver 0:8000
+echo "Pull Finished"
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
---
+++ scripts/nginx.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/bash
+
+sudo systemctl daemon-reload
+sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo cp /home/ubuntu/trans-crm-cicd/nginx/nginx.conf /etc/nginx/sites-available/users
+sudo ln -s /etc/nginx/sites-available/users /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+#sudo nginx -t
+sudo gpasswd -a www-data ubuntu
+sudo systemctl restart nginx
+
